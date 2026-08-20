8fcc1ec06121ad42e2f5ad1735b9959dc2cad5a7
SAI RAM PRAGNEY MURIKIPUDI <sairampragney@gmail.com>
2026-08-20 22:13:26 +0530

    Add multiplication_of_two_integers class

---
+++ Variables/multiplication_of_two_integers.java
@@ -0,0 +1,8 @@
+public class multiplication_of_two_integers {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 25;
+    int multiplication = a * b;
+    System.out.println(multiplication);
+  }
+}
