4170d4ebcacef65113aeaa08bdce486a3f07c67e
SAI RAM PRAGNEY MURIKIPUDI <sairampragney@gmail.com>
2026-08-20 21:50:38 +0530

    Add programs section for variable usage
    Added a section outlining various programs related to variable usage.

--- variables.java
+++ variables.java
@@ -11,3 +11,10 @@ This folder contains Java programs that demonstrate the use of variables to stor
 * Different data types
 * Using variables in expressions
 
+## Programs
+
+* Declaring and initializing variables
+* Printing variable values
+* Performing operations using variables
+* Updating variable values
+* Using variables with different data types
