dd05a9132ea051f40e60254a75ca9057758abf03
SAI RAM PRAGNEY MURIKIPUDI <sairampragney@gmail.com>
2026-08-21 14:30:41 +0530

    Add name_input class to read and greet user name

---
+++ input-statements/name_input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+  public class name_input {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      String name = sc.nextLine();
+      System.out.println("Hello" + " " + name);
+    }
+  }
