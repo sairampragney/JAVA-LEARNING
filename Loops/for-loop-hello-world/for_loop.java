08872f668ac1dcc9a0ad9395e088cc36360445f4
SAI RAM PRAGNEY MURIKIPUDI <sairampragney@gmail.com>
2026-08-23 20:13:14 +0530

    Add for loop to print 'Hello world' 100 times

---
+++ Loops/for-loop-hello-world/for_loop.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+  public class for_loop {
+    public static void main(String[] args) {
+      for (int counter = 0; counter < 100; counter = counter + 1){
+        System.out.println("Hello world");
+      }
+    }
+  }
