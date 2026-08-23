12bd7b72d6b2babba733f660c0e33b6880cf14d3
SAI RAM PRAGNEY MURIKIPUDI <sairampragney@gmail.com>
2026-08-23 22:50:39 +0530

    Add for loop to print numbers from 0 to 10

---
+++ Loops/for_loop_printing_numbers/for_numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class for_numbers {
+    public static void main(String[] args) {
+      for(int counter = 0; counter < 11; counter = counter+1){
+        System.out.println(counter);
+      }
+    }
+}
