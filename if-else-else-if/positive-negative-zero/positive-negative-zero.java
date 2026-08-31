0d6998b88a2fe8b89ee30e24db4b7eb224f34ad1
SAI RAM PRAGNEY MURIKIPUDI <sairampragney@gmail.com>
2026-08-31 20:50:33 +0530

    Update positive-negative-zero.java

--- if-else-else-if/positive-negative-zero/positive-negative-zero.java
+++ if-else-else-if/positive-negative-zero/positive-negative-zero.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class positive-negative-zero {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       int a = sc.nextInt();
