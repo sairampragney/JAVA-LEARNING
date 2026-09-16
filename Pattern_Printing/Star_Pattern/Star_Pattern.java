e8e2b10e283bde16961e7931d776ed98cdd2bd9d
SAI RAM PRAGNEY MURIKIPUDI <sairampragney@gmail.com>
2026-09-16 23:22:52 +0530

    Add Star_Pattern.java for printing stars
    Implement a program to print a star pattern in Java.

---
+++ Pattern_Printing/Star_Pattern/Star_Pattern.java
@@ -0,0 +1,18 @@
+public class Star_Pattern {
+  
+  public static void main(String[] args) {
+    
+    for (int i = 1; i <= 4; i++) {
+      
+      for (int j = 1; j < 5; j++) {
+        
+        System.out.print(" * ") ;
+        
+      }
+      
+      System.out.println(" * ") ; 
+    }
+    
+  }
+  
+}
